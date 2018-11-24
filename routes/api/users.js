7468e6230692f31a0288068362844e76ab604959
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-11-23 17:41:13 -0800

    registration models and server

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+//load user model
+const User = require('../model/User')
+
 router.get('/test', (req, res) => res.json({msg: "users works"}));
+
+//@route GET api/users/test
+//@desc (discription) Tests users route
+//@access Public
+router.get('/test', (req, res) => res.json({msg: "Users works"});
+
+//@route GET api/users/register
+//@desc (discription) register users 
+//@access Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if(user) {
+      return res.status(400).json({ email: 'already exists'});
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+
+      });
+    }
+  })
+});
+
 
 module.exports = router;
