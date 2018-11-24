5744711841df018efcca28aff1872d516a0b4697
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-11-23 16:49:16 -0800

    model new

---
+++ model/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//create schema
+const UserSchema = new Schema({
+//set a field
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('users', UsersSchema);
