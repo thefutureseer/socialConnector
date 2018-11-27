d174702611f5200719d78b7b58805bbafc0eca7a
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-11-27 15:51:55 -0800

    continue validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -30,11 +30,12 @@ router.post('/register', (req, res) => {
 
   User.findOne({ email: req.body.email }).then(user => {
     if(user) {
-      return res.status(400).json({ email: 'Email already exists'});
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', //size
-        r: 'pg', //rating
+        r: 'g', //rating
         d: 'mm' //default
       });
 
