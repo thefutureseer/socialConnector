f218f1afe6a59ac89d64f825e30190dd6ddc3882
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-11-29 14:38:26 -0800

    constructing profile

--- models/Profile.js
+++ models/Profile.js
@@ -8,8 +8,113 @@ const ProfileSchema = new Schema({
     ref: 'users'
   },
   handle: {
-    type: string,
+    type: String,
     required: true,
     max: 40
-  } 
-});
+  },
+  company: { 
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+    title: {
+      type: String,
+    required: true
+    },
+    company: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String
+    },
+    from: {
+      type: String,
+      required: true
+    },
+    to: {
+      type: Date,
+    },
+    current: {
+      type: Boolean,
+      default: false
+    },
+    discription: {
+    type: String
+    }
+  }],
+  experience: [
+    {
+    school: {
+      type: String,
+    required: true
+    },
+    degree: {
+      type: String,
+      required: true
+    },
+    fieldofstudy: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: String,
+      required: true
+    },
+    to: {
+      type: Date,
+    },
+    current: {
+      type: Boolean,
+      default: false
+    },
+    discription: {
+     type: String
+    }
+  }
+  ],
+  social: {
+   youtube: {
+    type: String
+   },
+   facebook: {
+    type: String
+   },
+   twitter: {
+    type: String
+   },
+   linkedin: {
+    type: String
+   },
+   instagram: {
+    type: String
+   }
+  },
+      date: {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+module.exports = Profile = mongoose.model('Profile', ProfileSchema)
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
+//load profile model
+const Profile = require('../../models/profile');
+//load user profile
+const User = require('../../models/User');
+
+// @route GET api/profile/test
+// description test profile route
+// @access Public
 router.get('/test', (req, res) => res.json({msg: "profile works"}));
+
+// @route GET api/profile/profile
+// description  get current users profile
+// @access Private
+router.get('/', passport.authenticate('JWT', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+   .then(profile => {
+     
+   })
+});
+
 
 module.exports = router;
