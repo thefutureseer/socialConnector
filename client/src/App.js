d37fa6f319a449732ad44d7ff912f229c4617892
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-12-15 10:30:04 -0800

    Navbar and begin look

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>My react app</h1>
+       <Navbar />
+       <h1>Dans New One</h1>
       </div>
     );
   }
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className = "navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+         <div className = "container">
+          <a className = "navbar-brand" href = "landing.html">SocialConnectors</a>
+          <button className = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "#mobile=nav">
+           <span className = "navbar-toggler-icon"></span>
+          </button>
+
+          <div className = "collapse navbar-collapse" id = "mobile-nav">
+            <ul className = "navbar-nav mr-auto">
+             <li className = "nav-item">
+              <a className = "nav-link" href = "profiles.html"> Media
+              </a>
+             </li>
+            </ul>
+
+            <ul className = "navbar-nav ml-auto">
+             <li className = "nav-item">
+              <a className = "nav-link" href = "register.html"> no sign Up</a>
+             </li>
+             <li className = "nav-item">
+              <a className = "nav-link">No Login</a>
+             </li>
+            </ul>
+
+          </div>
+         </div>
+        </nav>
+
+      </div>
+    )
+  }
+}
+
+export default Navbar;
+
+
