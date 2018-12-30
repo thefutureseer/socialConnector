1ca0ec948b73639b3700c0276014bc41f3cdc8af
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-12-29 16:50:11 -0800

    errors state props

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions'; 
@@ -15,7 +14,12 @@ class Register extends Component {
       password2: '',
       errors: {}
     };
+  }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({errors: nextProps.errors})
+    }
   }
 
   onChange = (e) => {
@@ -32,18 +36,13 @@ class Register extends Component {
     };
 
     this.props.registerUser(newUser);
-
-    // axios.post('/api/users/register', newUser)
-    //  .then(res => console.log(res.data))
-    //  .catch(err => this.setState({errors: err.response.data}));
   }
 
   render() {
     const { errors } = this.state;
-    const { user } = this.props.auth;
+   
     return (
       <div className="register">
-       {user ? user.name : null}
         <div className="container">
           <div className="row">
            <div className="col-md-8 m-auto">
@@ -129,10 +128,13 @@ class Register extends Component {
 }
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
- auth: state.auth
+ auth: state.auth,
+ errors: state.errors
 });
+
 export default connect(mapStateToProps, { registerUser })(Register);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
+//errors reducer
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import errorReducer from './errorReducer';
 
 export default combineReducers({
-   auth: authReducer
+   auth: authReducer,
+   errors: errorReducer
 });
