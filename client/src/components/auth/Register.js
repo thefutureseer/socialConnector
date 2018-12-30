0936d9ffc95b139da2b5d2c04a11b376223c66a7
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-12-30 08:16:38 -0800

    sign up through redux and redirect to login

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions'; 
@@ -35,7 +36,7 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    this.props.registerUser(newUser);
+    this.props.registerUser(newUser, this.props.history);
   }
 
   render() {
@@ -137,4 +138,4 @@ const mapStateToProps = (state) => ({
  errors: state.errors
 });
 
-export default connect(mapStateToProps, { registerUser })(Register);
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
