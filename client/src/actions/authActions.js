509cb36033cd528225c0f8cca5e527e76356ec22
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-12-31 14:10:48 -0800

    add errors and authentication action through reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import { GET_ERRORS }  from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER }  from './types';
 
 //Register user
 export const registerUser = (userData, history) => dispatch => {
@@ -13,3 +15,37 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+//Login user - get user token
+export const loginUser = userData => dispatch => {
+ axios.post('/api/users/login', userData)
+  .then(res => {
+// Save to local storage
+   const {token} = res.data;
+
+//set token to local storage
+    localStorage.setItem('jwt', token);
+
+//set token to auth header
+    setAuthToken(token);
+
+//decode token to get user data
+    const decoded = jwt_decode(token);
+//set current user
+    dispatch(setCurrentUser(decoded));
+
+  })
+  .catch(err => 
+    dispatch({ 
+    type: GET_ERRORS, 
+    payload: err.response.data
+  }));
+};
+
+//set logged in user
+  export const setCurrentUser = (decoded) => {
+    return {
+      type: SET_CURRENT_USER,
+      payload: decoded
+    };
+  };
