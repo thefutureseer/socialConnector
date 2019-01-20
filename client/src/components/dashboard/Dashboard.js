e5defa176e0156d66803165ce091e527649302e3
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-01-19 21:29:29 -0800

    begin dashboard proptypes state loading up to spinner

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -8,11 +8,41 @@ class Dashboard extends Component {
    this.props.getCurrentProfile();
  }
  render() {
+   const { user } = this.props.auth;
+   const { profile, loading} = this.props.profile;
+
+   let dashboardContent;
+
+   if(profile === null || loading) {
+    dashboardContent = <h4>Loading...</h4>
+   }else{
+    dashboardContent = <h4>Hello</h4>
+   }
+
    return (
-     <div>
-       <h1>Dashboard</h1>
+    <div className ="dashboard">
+     <div className ="container">
+      <div className ="row">
+       <div className ="col-md-12">
+        <h1 className ="display-4"> Dashboard</h1>
+        {dashboardContent}
+       </div>
+      </div>
      </div>
+    </div>
    );
- }
+ } 
 }
-export default connect(null, { getCurrentProfile })(Dashboard);
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
