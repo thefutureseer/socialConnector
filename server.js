7dd3e569b14c6f7f40c1660805cfc7496f27d474
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-08-16 21:02:32 -0700

    clean up

--- server.js
+++ server.js
@@ -2,38 +2,48 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-const gravatar = require('gravatar');
 
 const app = express();
 
-//body parser middleware 2 peices
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//DB config
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-//connect to mongo DB
+// Connect to MongoDB
 mongoose
-.connect(db)
-.then(()=> console.log('mongoDB connected'))
-.catch(err=> console.log(err));
+  .connect(db, { useNewUrlParser: true }) // no more deprecation warrning :)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-// USe routes
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-//passport middleware
-app.use(passport.initialize());
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-// passport config with jwt strategy
-require('./config/passport')(passport);
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server running on Port ${port}`) );
+app.listen(port, () => console.log(`Server running on port ${port}`));
